72150bf1ecaefd0f8aa62ad06da8ca79f73c4f9c
Simon <simonfong6@gmail.com>
2018-06-02 22:37:45 -0700

    Added capability to clear markers

--- gps_client/update_server.py
+++ gps_client/update_server.py
@@ -13,6 +13,9 @@ URL = 'http://not-kiwi-bot.mothakes.com'
 # File that stores the GPS markers
 DATA_FILE = 'data.json'
 
+# Empty points
+CLEAR_FILE = 'clear.json'
+
 project_gps_waypoint_1 = {
                 "position": {
                     "lat": 32.8811271, 
@@ -27,7 +30,32 @@ project_gps_waypoint_2 = {
                     "lng": -117.2374792
                 }, 
                 "label": "P GPS Goal 2"
-              }              
+              }
+              
+
+start =       {
+                "position": {
+                    "lat": 32.881322,
+                    "lng": -117.235454
+                }, 
+                "label": "START"
+              }
+goal =       {
+                "position": {
+                    "lat": 32.881162,
+                    "lng": -117.235459
+                }, 
+                "label": "GOAL"
+              }
+              
+"""
+lng diff: 0.000005
+distance: 18.29 m
+3.7 m / 0.000001
+
+More negative is south.
+
+"""            
 
 def add(marker):
     r = requests.post(URL + '/markers/add', json = {'marker':marker})
@@ -35,13 +63,19 @@ def add(marker):
     
 def replace(markers):
     requests.post(URL + '/markers/replace', json = {'markers':markers})
+    
+def clear():
+    with open(CLEAR_FILE, 'r+') as f:
+            data = json.load(f)
+            markers = data['markers']
+            replace(markers)
 
 
 def _main():
     from sys import argv
     if(len(argv) < 2):
         print("Not enough arguments.")
-        print("Options: replace, gps1, gps2")
+        print("Options: replace, clear, gps1, gps2, start, goal")
         return
     
     option = argv[1]
@@ -54,6 +88,11 @@ def _main():
             print("Replaced markers at {}".format(URL))
         return
     
+    if(option == 'clear'):
+        clear()
+        print("Cleared markers at {}".format(URL))
+        return
+    
     if(option == 'gps1'):
         add(project_gps_waypoint_1)
         print("Added ProjectGPS waypoint 1 at {}".format(URL))
@@ -63,7 +102,17 @@ def _main():
         add(project_gps_waypoint_2)
         print("Added ProjectGPS waypoint 2 at {}".format(URL))
         return
-        
+    
+    if(option == 'start'):
+        add(start)
+        print("Added start waypoint at {}".format(URL))
+        return
+    
+    if(option == 'goal'):
+        add(goal)
+        print("Added goal waypoint at {}".format(URL))
+        return
+    
     print("'{}' is not a valid option.".format(option))
     
     
