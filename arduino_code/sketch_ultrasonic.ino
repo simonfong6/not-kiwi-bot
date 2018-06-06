6e215d0d7efd583afc085266a06591108a98d913
Saurabh Kulkarni <saurabh.kulkarni997@gmail.com>
2018-06-06 12:44:58 -0700

    ultrasonics work!

--- arduino_code/sketch_ultrasonic.ino
+++ arduino_code/sketch_ultrasonic.ino
@@ -1,3 +1,5 @@
+
+
 int trigPins[] = {2,9};
 int echoPins[] = {3,10};
 //const int trigPin1 = 2;
@@ -8,14 +10,14 @@ int echoPins[] = {3,10};
 
 long duration;
 int newDistCm;
-int responseTime;
-const int MAX_DIST = 500; //cm
+const int MAX_DIST = 300; //cm
 int oldVals[] = {MAX_DIST, MAX_DIST};
-//const int timeout = MAX_DIST * 2 /0.034;
-const int TIMEOUT = //TODO;
+const int timeout = MAX_DIST * 2 /0.034;
+const int TIMEOUT = 12;
 unsigned long mytime;
+unsigned long responseTime;
 //increase to filter more and reduce sensitivity 
-const double LOW_PASS_ALPHA = 0.3;
+const double LOW_PASS_ALPHA = 0.6;
 void setup() {
   
   pinMode(trigPins[0], OUTPUT);
@@ -36,29 +38,25 @@ double getDist(int pin) {
   mytime = millis();
   duration = pulseIn(echoPins[pin], HIGH, timeout);
   responseTime = millis() - mytime;
-  //TODO: READ WHAT VALUE THIS PRINTS ON TIMEOUT
-  //AND CHANGE THE VALUE OF TIMEOUT ACCORDINGLY
-  //THEN DELETE THIS PRINT
-  Serial.println(responseTime);
+  //Serial.println(responseTime);
   if (responseTime >= TIMEOUT) {
-    newDistCm = 500;
+    newDistCm = MAX_DIST;
   }
   else {
     newDistCm = duration*0.034/2;
   }
-  //oldVals[pin] = LOW_PASS_ALPHA * oldVals[pin] + (1-LOW_PASS_ALPHA)*newDistCm;
-  return newDistCm;
+  oldVals[pin] = LOW_PASS_ALPHA * oldVals[pin] + (1-LOW_PASS_ALPHA)*newDistCm;
+  return oldVals[pin];
 }
 
 void loop() {
-  newDistCm= getDist(0);
-  String dist0 = String(newDistCm);
+newDistCm= getDist(0);
+String dist0 = String(newDistCm);
 
-  delayMicroseconds(10000);
+delayMicroseconds(10000);
 
-  newDistCm = getDist(1);
-  String both = dist0 + " " + String(newDistCm);
-  Serial.println(both);
-  delayMicroseconds(10000);
-
+newDistCm = getDist(1);
+String both = dist0 + " " + String(newDistCm);
+Serial.println(both);
+delayMicroseconds(10000);
 }
